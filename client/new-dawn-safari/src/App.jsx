e1dc4f310dfcc04c5018774cf8c1bea6a71e4dfe
Dtech <dhuhuinc@gmail.com>
2026-04-23 16:44:44 +0300

    just ran build3

--- client/new-dawn-safari/src/App.jsx
+++ client/new-dawn-safari/src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
+
+
 import Home from './pages/Home'
 import Booking from './pages/Booking'
 import Contact from './pages/Contact'
